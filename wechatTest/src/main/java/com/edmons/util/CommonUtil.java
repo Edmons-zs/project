a441f5fff86f5734f2f446fe4a62b6f1e293c3f6
edmons <zhangs0321@163.com>
2018-05-28 00:02:47 +0800

    feat:初始项目

---
+++ wechatTest/src/main/java/com/edmons/util/CommonUtil.java
@@ -0,0 +1,12 @@
+package com.edmons.util;
+
+public class CommonUtil {
+
+    public void getUserInfo(){
+        System.out.println("获取用户信息");
+    }
+
+    public void getAccountInfo(){
+        System.out.println("获取账户信息");
+    }
+}
